84c1cabc654e68d2d4bf474b2cbbfe4bd2f0e4ce
rush <rush@wusaby.com>
2024-06-09 02:58:46 +0300

    refactor: remove manual theme switcher (use dark only)

--- app/routes/_shell/route.tsx
+++ app/routes/_shell/route.tsx
@@ -1,23 +1,28 @@
-import { ThemeSwitcher } from './components/theme-switcher'
+// import { ThemeSwitcher } from './components/theme-switcher'
 import { Trans } from '@lingui/macro'
 
 export function Component() {
   return (
-    <div className="h-full w-11/12 md:w-8/12 m-auto">
-      <div className="h-full flex flex-col">
-        <header className="flex h-16 items-center">
-          <Link to="/">
-            <h1 className="text-2xl font-extrabold italic tracking-tight">
-              <Trans>YemenCar.</Trans>
-            </h1>
-          </Link>
-          <div className="ml-auto flex items-center space-x-4">
-            <ThemeSwitcher />
-          </div>
-        </header>
-
-        <Outlet />
+    <>
+      <div className="h-full w-11/12 md:w-8/12 m-auto">
+        <div className="h-full flex flex-col">
+          <header className="flex h-16 items-center">
+            <Link to="/">
+              <h1 className="text-2xl font-extrabold italic tracking-tight">
+                <Trans>YemenCar.</Trans>
+              </h1>
+            </Link>
+            <div className="ml-auto flex items-center space-x-4">
+              {/* <ThemeSwitcher /> */}
+            </div>
+          </header>
+        </div>
       </div>
-    </div>
+      <div className="h-full w-11/12 md:w-8/12 m-auto">
+        <div className="h-full flex flex-col">
+          <Outlet />
+        </div>
+      </div>
+    </>
   )
 }
