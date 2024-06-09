d68c609d5b740670c340528d854922cbf27b0b08
rush <rush@wusaby.com>
2024-06-09 10:58:48 +0300

    refactor: split images in detalis page to gallery

---
+++ app/routes/_shell.list.$id/components/gallery.tsx
@@ -0,0 +1,34 @@
+export function Gallery() {
+  const car = useLoaderData() as Car
+  const [selectedImage, setSelectedImage] = useState(car.images[0])
+
+  return (
+    <section>
+      <div className="flex flex-col gap-2">
+        <img
+          alt="main image"
+          className="w-full"
+          src={selectedImage}
+        />
+        <div className="flex gap-2 w-full">
+          {car.images.map((image) => (
+            <div
+              className="relative cursor-pointer"
+              key={image}
+              onClick={() => setSelectedImage(image)}
+            >
+              <img
+                alt="image"
+                className="flex-shrink-0 w-[100px]"
+                src={image}
+              />
+              <div
+                className={`absolute inset-0  ${image === selectedImage && 'dark:bg-gray-900/60'}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
--- app/routes/_shell.list.$id/route.tsx
+++ app/routes/_shell.list.$id/route.tsx
@@ -1,3 +1,4 @@
+import { Gallery } from './components/gallery'
 import { type LoaderFunctionArgs } from 'react-router-dom'
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -13,36 +14,9 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 export function Component() {
-  const car = useLoaderData() as Car
-  const [selectedImage, setSelectedImage] = useState(car.images[0])
-
   return (
-    <section>
-        <div className="flex flex-col gap-2">
-          <img
-            alt="main image"
-            className="w-full"
-            src={selectedImage}
-          />
-          <div className="flex gap-2 w-full">
-            {car.images.map((image) => (
-              <div
-                className="relative cursor-pointer"
-                key={image}
-                onClick={() => setSelectedImage(image)}
-              >
-                <img
-                  alt="image"
-                  className="flex-shrink-0 w-[100px]"
-                  src={image}
-                />
-                <div
-                  className={`absolute inset-0  ${image === selectedImage && 'dark:bg-gray-900/60'}`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
+    <div>
+      <Gallery />
+    </div>
   )
 }
