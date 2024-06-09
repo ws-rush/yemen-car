619b1bc460838353c60331387f628fff52c32eda
rush <rush@wusaby.com>
2024-06-09 11:11:05 +0300

    feat: display car's main data

--- app/routes/_shell.list.$id/route.tsx
+++ app/routes/_shell.list.$id/route.tsx
@@ -14,9 +14,21 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 export function Component() {
+  const car = useLoaderData() as Car
+
   return (
     <div>
       <Gallery />
+
+      <section className="flex flex-col gap-2 my-6">
+        <h1 className="text-4xl font-semibold">
+          {car.company} {car.model}
+        </h1>
+        <div className="flex gap-5 items-center">
+          <Badge>{`${car.status}`}</Badge>
+          <strong className="text-lg">${car.price}</strong>
+        </div>
+      </section>
     </div>
   )
 }
